27759e8b947af4701c387e0a3111712d49d0b91d
yanis-kharouni <yanis.kharouni@ens.uvsq.fr>
2021-01-26 18:17:12 +0100

    hdf5 added in the same file test

--- simpleui/src/screenprinter.cpp
+++ simpleui/src/screenprinter.cpp
@@ -58,47 +58,6 @@ void ScreenPrinter::simulation_updated( const Distributed2DField& data )
 void ScreenPrinter::simulation_updated( const Distributed2DField& data )
 {
 
-	//declarer tableau pour stocker le resultat localement
-	double *resultat[data.distribution().extent( DX )][data.distribution().extent( DY )];
-
-	//stocker les resultats
-
-	
-	if ( data.distribution().rank() == 0 ) {
-		cout << "at t="<<data.time()<<" : [" << endl;
-	}
-	cout<<flush; 
-	MPI_Barrier(data.distribution().communicator());
-	sleep_for(milliseconds(1));
-	for ( int pyy = data.distribution().extent( DY )-1; pyy >=0 ; --pyy ) {
-		for ( int yy = data.noghost_view().extent( DY )-1; yy >=0 ; --yy ) {
-			for ( int pxx = 0; pxx < data.distribution().extent( DX ); ++pxx ) {
-				if ( data.distribution().coord( DX ) == pxx && data.distribution().coord( DY ) == pyy ) {
-					if ( pxx == 0 ) {
-						cout << "  [";
-					}
-					for ( int xx = 0; xx < data.noghost_view().extent( DX ); ++xx ) {
-						if ( 0 == data.noghost_view(yy, xx) ) {
-							cout << " .";
-						} else {
-							cout << " " << data.noghost_view(yy, xx);
-						}
-					}
-					if ( pxx == data.distribution().extent( DX ) - 1 ) {
-						cout << " ]\n";
-					}
-				}
-				cout<<""<<flush;
-				MPI_Barrier(data.distribution().communicator());
-				sleep_for(milliseconds(1));
-			}
-		}
-	}
-	if ( data.distribution().rank() == 0 ) {
-		cout << "]" << endl;
-	}
-	cout<<flush;
-	
 		const string file_name = "my_file.h5";
 	
 	// create the file access property list
